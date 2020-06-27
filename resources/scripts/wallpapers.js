6c1913b9790258b8cc8f2cc84cbae42301c6597a
losbiw <skorinov41@gmail.com>
2020-06-28 00:05:18 +0300

    Fix wallpaper module path

--- resources/scripts/wallpapers.js
+++ resources/scripts/wallpapers.js
@@ -1,6 +1,6 @@
 const { join, resolve } = require('path');
 const exec = require('child_process').execFile;
-const execPath = join(__dirname, '../app/Wallpaper.exe');
+const execPath = join(__dirname, '../app/wallpaper.exe');
 
 async function set(imgPath){
     if(typeof imgPath !== 'string')
