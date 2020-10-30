3721f307fe157f6dabaaa0f32880a4cb9db2d7bd
losbiw <skorinov41@gmail.com>
2020-10-30 03:43:44 +0200

    Update webpack config override

--- src/config/config-override.js
+++ src/config/config-override.js
@@ -1,11 +1,19 @@
-module.exports = function override(config, _env) { 
-  config.module.rules[1].use[0].options.useEslintrc = true;
+const { override, useEslintRc } = require("customize-cra");
+const { resolve } = require('path');
+
+const customizer = () => config => {
   config.node.__dirname = true;
   
-  config.module.rules.unshift({
+  config.module.rules.splice(2, 0, {
     test: /\.svg(\?raw)$/,
     loader: 'raw-loader'
   });
 
   return config;
 }
+
+module.exports = override(
+  useEslintRc(resolve(__dirname, '.eslintrc')),
+  customizer()
+)
+
