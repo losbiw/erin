23296284966fdf30002fb8d01ce15947ad013216
losbiw <skorinov41@gmail.com>
2021-04-23 08:42:10 +0300

    Try linting some modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import React, {Component} from 'react'
-import User from './Components/User/User'
-import Setup from './Components/Setup/Setup'
-import Controls from './Components/Controls/Controls'
-import { Warning, CustomWarning } from './Components/Warning/Warning'
-import Update from './Components/Update/Update'
+import React, { Component } from 'react';
+import config from '@modules/config';
+import OS from '@modules/OS';
+import { fetchGeocoding } from '@modules/APIs';
+import { Theme } from '@/interfaces/Config';
+import { Warning as WarningInterface } from '@interfaces/Warning.d';
+import User from './Components/User/User';
+import Setup from './Components/Setup/Setup';
+import Controls from './Components/Controls/Controls';
+import { Warning, CustomWarning } from './Components/Warning/Warning';
+import Update from './Components/Update/Update';
 
-import config from '@modules/config'
-import OS from '@modules/OS'
-import { fetchGeocoding } from '@modules/APIs'
-
-import { Theme } from '@/interfaces/Config'
-import { Warning as WarningInterface } from '@interfaces/Warning.d'
-
-import './App.scss'
-import './style/global.scss'
+import './App.scss';
+import './style/global.scss';
 
 const { ipcRenderer } = window.require('electron');
 
@@ -25,115 +23,129 @@ interface State{
     warning: string | WarningInterface,
 }
 
-export default class App extends Component<{}, State>{
-    state: State = {
-        theme: Theme.Dark,
-        isUpdateAvailable: false,
-        isRequiredFilled: false,
-        isComplete: null,
-        warning: ''
+export default class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      theme: Theme.Dark,
+      isUpdateAvailable: false,
+      isRequiredFilled: false,
+      isComplete: null,
+      warning: '',
     };
+  }
 
-    async componentDidMount(){
-        const { theme, isComplete, isFirstTime } = config.get();
-        const location = await fetchGeocoding(() => {});
+  async componentDidMount() {
+    const { theme, isComplete, isFirstTime } = config.get();
+    const location = await fetchGeocoding(() => {});
 
-        if(isFirstTime || typeof isFirstTime === 'undefined'){
-            await fetch('https://erin-downloads.herokuapp.com/api/increase', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    name: OS.define(),
-                    location: location.toLowerCase()
-                })
-            });
-            
-            config.set({ isFirstTime: false });
-        }
+    if (isFirstTime || typeof isFirstTime === 'undefined') {
+      await fetch('https://erin-downloads.herokuapp.com/api/increase', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: OS.define(),
+          location: location.toLowerCase(),
+        }),
+      });
 
-        ipcRenderer.send('component-did-mount');
-
-        ipcRenderer.on('update-is-available', () => {
-            this.setState({
-                isUpdateAvailable: true
-            });
-        });
-
-        this.setState({
-            theme,
-            isComplete,
-            isRequiredFilled: isComplete
-        });
+      config.set({ isFirstTime: false });
     }
 
+    ipcRenderer.send('component-did-mount');
+
+    ipcRenderer.on('update-is-available', () => {
+      this.setState({
+        isUpdateAvailable: true,
+      });
+    });
+
+    this.setState({
+      theme,
+      isComplete,
+      isRequiredFilled: isComplete,
+    });
+  }
+
     switchTheme = (): void => {
-        const current = this.state.theme;
-        const value = current === Theme.Dark ? Theme.Light : Theme.Dark;
+      const { theme: current } = this.state;
+      const value = current === Theme.Dark ? Theme.Light : Theme.Dark;
 
-        const updated = {
-            theme: value
-        }
+      const updated = {
+        theme: value,
+      };
 
-        config.set(updated);
-        this.setState(updated);
+      config.set(updated);
+      this.setState(updated);
     }
 
     rejectUpdate = (): void => {
-        this.setState({
-            isUpdateAvailable: false
-        });
+      this.setState({
+        isUpdateAvailable: false,
+      });
     }
 
     setIsRequiredFilled = (isRequiredFilled: boolean): void => {
-        this.setState({
-            isRequiredFilled
-        });
+      this.setState({
+        isRequiredFilled,
+      });
     }
 
     setIsComplete = (isComplete: boolean): void => {
-        this.setState({
-            isComplete
-        });
+      this.setState({
+        isComplete,
+      });
     }
 
     setWarning = (warning: string | WarningInterface): void => {
-        this.setState({
-            warning
-        });
+      this.setState({
+        warning,
+      });
     }
 
-    render(){
-        const { switchTheme, setWarning, rejectUpdate, setIsComplete, setIsRequiredFilled } = this;
-        const { theme, isComplete, warning, isRequiredFilled, isUpdateAvailable } = this.state;
+    render() {
+      const {
+        switchTheme, setWarning, rejectUpdate, setIsComplete, setIsRequiredFilled,
+      } = this;
+      const {
+        theme, isComplete, warning, isRequiredFilled, isUpdateAvailable,
+      } = this.state;
 
-        return(
-            <div className={ `theme ${theme}` }>
-                <Controls />
+      return (
+        <div className={`theme ${theme}`}>
+          <Controls />
 
-                { isComplete !== null
-                    ? isComplete && isRequiredFilled
-                    ? <User theme={ theme } 
-                            setWarning={ setWarning } 
-                            switchTheme={ switchTheme }
-                            setIsComplete={ setIsComplete }/> 
-                    : <Setup theme={ theme }
-                            isComplete={ isComplete }
-                            switchTheme={ switchTheme }
-                            setWarning={ setWarning }
-                            setIsComplete={ setIsComplete }
-                            setIsRequiredFilled={ setIsRequiredFilled }/>
-                    : <></>  
-                }
+          { isComplete !== null
+            ? isComplete && isRequiredFilled
+              ? (
+                <User
+                  theme={theme}
+                  setWarning={setWarning}
+                  switchTheme={switchTheme}
+                  setIsComplete={setIsComplete}
+                />
+              )
+              : (
+                <Setup
+                  theme={theme}
+                  isComplete={isComplete}
+                  switchTheme={switchTheme}
+                  setWarning={setWarning}
+                  setIsComplete={setIsComplete}
+                  setIsRequiredFilled={setIsRequiredFilled}
+                />
+              )
+            : <></>}
 
-                { isUpdateAvailable && <Update rejectUpdate={ rejectUpdate } setWarning={ setWarning } /> }
+          { isUpdateAvailable && <Update rejectUpdate={rejectUpdate} setWarning={setWarning} /> }
 
-                { (warning && typeof warning === 'string')
-                    ? <Warning warning={ warning } removeWarning={ () => setWarning('') }/>
-                    : warning && <CustomWarning warning={ warning as WarningInterface } removeWarning={ () => setWarning('') } />
-                }
-            </div>
-        )
+          { (warning && typeof warning === 'string')
+            ? <Warning warning={warning} removeWarning={() => setWarning('')} />
+            : warning && <CustomWarning warning={warning as WarningInterface} removeWarning={() => setWarning('')} />}
+        </div>
+      );
     }
 }
--- src/modules/APIs.ts
+++ src/modules/APIs.ts
@@ -1,115 +1,108 @@
-//change all error handlers
-import Weather from '@interfaces/Weather'
+// change all error handlers
+import Weather from '@interfaces/Weather';
 
-//add types to fetchAPI, fetchPexels
+// add types to fetchAPI, fetchPexels
 
+const fetchAPI = async (url: string, handleError: (err: number) => void, headers?: HeadersInit) => {
+  try {
+    const req = await fetch(url, {
+      method: 'GET',
+      headers,
+    });
+    const res = await req.json();
+    return res;
+  } catch {
+    handleError(502);
+  }
+};
 
-const fetchAPI = async(url: string, handleError: (error: number) => void, headers?: HeadersInit) => {
-    try{
-        const req = await fetch(url, {
-            method: "GET",
-            headers
-        })
-        const res = await req.json();
-        return res
+const fetchWeather = (handleError: (error: number) => void): Promise<Weather | undefined> => {
+  const callback = async (res: Function, position: any): Promise<void> => {
+    try {
+      const key = window.process.env.WEATHER_API_KEY;
+      const { latitude: lat, longitude: lng } = position.coords;
+
+      const json = await fetchAPI(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}`,
+        handleError,
+      );
+
+      const { weather, sys } = json;
+      const formatted = {
+        main: weather[0].main,
+        time: sys,
+      };
+
+      res(formatted);
+    } catch (err) {
+      handleError(503);
     }
-    catch{
-        handleError(502);
-    }
-}
+  };
 
-const fetchWeather = (handleError: (error: number) => void): Promise<Weather | undefined> => { 
-    const callback = async(res: Function, position: any): Promise<Weather | undefined> => { //change mb pridumat chto-to
-        try{
-            const key = window.process.env.WEATHER_API_KEY;
-            const { latitude: lat, longitude: lng } = position.coords;
-        
-            const json = await fetchAPI(
-                `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}`,
-                handleError
-            );
-            
-            const { weather, sys } = json;
-            const formatted = {
-                main: weather[0].main,
-                time: sys
-            }
-            
-            res(formatted);
-        }
-        catch(err){
-            handleError(503);
-
-            return
-        }
-    }
-
-    return fetchGeolocation(callback)
-}
+  return fetchGeolocation(callback);
+};
 
 const fetchGeocoding = (handleError: (error: number) => void): Promise<string> => {
-    const callback = async(res: Function, position: any): Promise<string> => { //change mb pridumat chto-to
-        try{
-            const key = window.process.env.GOOGLE_API_KEY;
-            const { latitude: lat, longitude: lng } = position.coords;
+  const callback = async (res: Function, position: any): Promise<string> => { // change mb pridumat chto-to
+    try {
+      const key = window.process.env.GOOGLE_API_KEY;
+      const { latitude: lat, longitude: lng } = position.coords;
 
-            const geocoding = await fetchAPI(
-                `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${key}&result_type=country&language=en`,
-                handleError
-            );
+      const geocoding = await fetchAPI(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${key}&result_type=country&language=en`,
+        handleError,
+      );
 
-            return res(geocoding.results[0].formatted_address);
-        }
-        catch(err){
-            handleError(503);
+      return res(geocoding.results[0].formatted_address);
+    } catch (err) {
+      handleError(503);
 
-            return res('');
-        }
+      return res('');
     }
+  };
 
-    return fetchGeolocation(callback);
-}
+  return fetchGeolocation(callback);
+};
 
-const fetchGeolocation = (callback: Function): Promise<any> => {
-    return new Promise(res => {
-        navigator.geolocation.getCurrentPosition(position => callback(res, position));
-    })
-}
+const fetchGeolocation = (callback: Function): Promise<any> => new Promise((res) => {
+  navigator.geolocation.getCurrentPosition((position) => callback(res, position));
+});
 
-const fetchPexels = async(keywords: string[], handleError: (error: number) => void) => {
-    const collection: any[] = []; //change mb pridumat chto-to
-    let canRequestMore = true;
-    
-    for(let key of keywords){
-        let page = 1;
-        const headers: HeadersInit = new Headers();
-        headers.set('Content-type', 'application/json');
-        headers.set('Authorization', window.process.env.PEXELS_API_KEY || '');
-        
-        while(canRequestMore && page < 3){
-            try{
-                const res = await fetchAPI(
-                    `https://api.pexels.com/v1/search?query=${key}&per_page=78&page=${page}`,  
-                    handleError,
-                    headers,
-                );
+const fetchPexels = async (keywords: string[], handleError: (error: number) => void) => {
+  const collection: any[] = []; // change mb pridumat chto-to
+  let canRequestMore = true;
 
-                const { photos } = await res;
-                
-                if(!res) return
-                if(photos.length < 78) canRequestMore = false
-    
-                page++;
-                collection.push(...photos);
-            }
-            catch{
-                handleError(429);
-                return false
-            }
-        }
+  for (const key of keywords) {
+    let page = 1;
+    const headers: HeadersInit = new Headers();
+    headers.set('Content-type', 'application/json');
+    headers.set('Authorization', window.process.env.PEXELS_API_KEY || '');
+
+    while (canRequestMore && page < 3) {
+      try {
+        const res = await fetchAPI(
+          `https://api.pexels.com/v1/search?query=${key}&per_page=78&page=${page}`,
+          handleError,
+          headers,
+        );
+
+        const { photos } = await res;
+
+        if (!res) return;
+        if (photos.length < 78) canRequestMore = false;
+
+        page++;
+        collection.push(...photos);
+      } catch {
+        handleError(429);
+        return false;
+      }
     }
+  }
 
-    return collection
-}
+  return collection;
+};
 
-export { fetchPexels, fetchWeather, fetchGeolocation, fetchGeocoding }
+export {
+  fetchPexels, fetchWeather, fetchGeolocation, fetchGeocoding,
+};
