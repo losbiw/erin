bec5991d20fc53224f27bef2a5a073aac83996a9
losbiw <skorinov41@gmail.com>
2021-04-25 03:01:04 +0300

    Split react build into smaller chunks

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,27 @@ module.exports = {
     minimizer: [
       new CssMinimizerPlugin(),
     ],
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
   node: {
     __dirname: true,
