b1b383028d6a3088864d61475b989083187aaac1
losbiw <skorinov41@gmail.com>
2021-04-25 20:27:34 +0300

    Remove v8 cache

--- electron/main.ts
+++ electron/main.ts
@@ -8,12 +8,11 @@ import defineOS from './os';
 import tray from './tray';
 import initIPCEvents from './ipcEvents';
 import initPowerEvents from './powerEvents';
-import initUpdateEvents from './updateEvents';
+// import initUpdateEvents from './updateEvents';
 
-require('dotenv').config({ path: join(__dirname, '.env') }); // change
-require('v8-compile-cache');
+require('dotenv').config({ path: join(__dirname, './.env') }); // change
 
-initUpdateEvents();
+// initUpdateEvents();
 
 let win: BrowserWindow;
 let winTray: Tray;
