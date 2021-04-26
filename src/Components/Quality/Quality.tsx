1f503ba4eb7b3c4513b45ab3444db8b272d1e414
losbiw <skorinov41@gmail.com>
2021-04-26 18:20:35 +0300

    Fix not working quality picker

--- src/Components/Quality/Quality.tsx
+++ src/Components/Quality/Quality.tsx
@@ -12,12 +12,18 @@ interface Props{
 const Quality: FC<Props> = (props: Props) => {
   const { changeQuality, initialQuality } = props;
 
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    changeQuality(value as QualityInterface);
+  };
+
   return (
     <select
       name="quality" // todo custom dropdown
       className="quality-select"
       data-value="value"
       defaultValue={initialQuality}
+      onChange={handleChange}
     >
       {
         options.map((option) => {
@@ -27,7 +33,6 @@ const Quality: FC<Props> = (props: Props) => {
             <option
               className="quality-option"
               value={value}
-              onClick={() => changeQuality(value)}
               key={value}
             >
               { capitalizeFirstLetter(label) }
