b601da7d84978a768d5b1c846432eefca1ad9fa6
losbiw <skorinov41@gmail.com>
2021-05-15 15:57:15 +0300

    Formatting change

--- src/modules/wallpaper/scripts.ts
+++ src/modules/wallpaper/scripts.ts
@@ -19,15 +19,15 @@ const linux = (imgPath: string): Distros => ({
   },
   kde: {
     set: `dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
-                var Desktops = desktops();                                                                                                                       
-                for (i=0;i<Desktops.length;i++) {
-                        d = Desktops[i];
-                        d.wallpaperPlugin = "org.kde.image";
-                        d.currentConfigGroup = Array("Wallpaper",
-                                                    "org.kde.image",
-                                                    "General");
-                        d.writeConfig("Image", "file://${imgPath}");
-                }'`,
+          var Desktops = desktops();                                                                                                                       
+          for (i=0;i<Desktops.length;i++) {
+            d = Desktops[i];
+            d.wallpaperPlugin = "org.kde.image";
+            d.currentConfigGroup = Array("Wallpaper",
+                                        "org.kde.image",
+                                        "General");
+            d.writeConfig("Image", "file://${imgPath}");
+          }'`,
   },
 });
 
