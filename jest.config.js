0975e8a87dbe49a8d431c8616be5a01b93bd7eb8
losbiw <skorinov41@gmail.com>
2021-05-17 19:04:04 +0300

    Update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,10 @@ module.exports = {
     '<rootDir>/src',
   ],
   moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
     '@interfaces(.*)$': '<rootDir>/src/interfaces$1',
+    '@ui(.*)$': '<rootDir>/src/assets/ui$1',
   },
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
