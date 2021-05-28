7d61e13ae51ec07138c5d9d94d2673d61fda6a7d
Vlad Skorinov <57506835+losbiw@users.noreply.github.com>
2021-05-28 21:57:36 +0300

    Delete Pages directory because it's a copy

--- src/Pages/Settings/items.ts
+++
@@ -1,9 +0,0 @@
-import Settings from '@interfaces/Settings';
-
-export default [
-  { name: Settings.Mode },
-  { name: Settings.Keywords },
-  { name: Settings.Timer },
-  { name: Settings.Startup },
-  { name: Settings.Quality },
-];
